1136be30df1c9e3660035f2e6776b197f6284f65
benjamin-holcombe <benjamin.holcombe@novaintelligence.com>
2015-01-25 17:04:44 -0500

    Initial Version
    makeCacheMatrix will accept a matrix variable and return a function
    where the matrix supplied (x) will be embeded in its environment. This
    returned function will have the set, get, setsolve and getsolve
    functions inside it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+#==================================================================================================
+#= Name:        cacheMatrix
+#= Created:     01-25-2015
+#= Author:      Ben Holcombe
+#= Description: makeCacheMatrix will accept a matrix variable and return a function where the
+#               matrix supplied (x) will be embeded in its environment. This returned function 
+#               will have the set, get, setsolve and getsolve functions inside it.
+#==================================================================================================
+#= Test:        
+# Creates a 3x3 matrix generating a random number for each cell
+# x <- matrix(runif(9, 5.0, 7.5), nrow=3, ncol=3)
+# Populate mtx
+# mtx <- makeCacheMatrix(x)
+# Calculate the solve (inverse) of the matrix
+# cacheSolve(mtx)
+# Print the two matrices
+# print(mtx$get())
+# Inverted Matrix
+# print(mtx$getsolve())
+#==================================================================================================
+#= Version:     1.0.000
+#= Revisions:
+#= Author       Date        Version Comments
+#= ============ =========== ======= ===============================================================
+#= bholcombe    01-25-2015  1.0.000 Initial Version
+#==================================================================================================
 
-## Write a short comment describing this function
+## makeCacheMatrix will accept a matrix variable and return a function where the matrix supplied (x)
+## will be embeded in its environment. This returned function will have the set, get, setsolve
+## and getsolve functions inside it.  
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  s <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  
+  # Returning a list of the following functions
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve accepts the product of makeCacheMatrix as input variable (x). The end result will be to return the solve (inverse)
+## of the matrix from the function as well as to pass the solve back to makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Retreive the getsolve function from x
+  m <- x$getsolve()
+  # If m is not null return m
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # Retreieve the data from get (original value supplied to makeCacheMatrix as x)
+  data <- x$get()
+  # Calculate the solve of data
+  m <- solve(data, ...)
+  # Pass m (inverse of originally supplied matrix) to the setsolve function (makeCacheMatrix will store this value)
+  x$setsolve(m)
+  # Return a matrix that is the inverse of 'x'
+  m
 }
